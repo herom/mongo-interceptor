6d8820fa2b0c66eab4db8ffbd2e3c055abeb4f2c
herom <h.romirer@gmail.com>
2017-03-06 22:41:24 +0100

    [DEV] Fixed `setReplacer()` function in order to set a custom replacement-string for detected mongodb-operators.

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ var interceptor = require('./lib/interceptor');
 
 module.exports = {
   VERSION: version,
+
   check: interceptor.checkStr,
+
   setReplacer: function setReplacer (replacer) {
     if (replacer.indexOf('$') === -1) {
-
+      interceptor.setOperatorReplacer(replacer);
     }
   }
 };
